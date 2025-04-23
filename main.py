10febad51c8ca854e0411cecea7558fbc94c895a
lainvasora <myrageburnsbright@gmail.com>
2025-04-23 13:53:26 -0400

    sync audio + video playing, r+ in .txt

--- main.py
+++ main.py
@@ -1,52 +1,115 @@
 import cv2
+import os
+import tempfile
+import threading
+import time
 from paddleocr import PaddleOCR
-import paddle
 import numpy as np
-cls = True
+from moviepy import VideoFileClip
+import pygame
+
+cls = True # angles
 ocr = PaddleOCR(cls=cls, lang='ru', use_gpu=True)
 
-cap = cv2.VideoCapture("dd2.mp4")
+video_path = "dd.mp4"
+audio_path = None
+
+temp_file = False
+
+# === Извлечение аудио во временный .wav файл ===
+if not audio_path:
+    clip = VideoFileClip(video_path)
+    if not clip.audio:
+        raise ValueError("❌ В видео нет аудио-дорожки!")
+
+    temp_audio_path = os.path.join(tempfile.gettempdir(), "temp_audio.wav")
+
+    print(temp_audio_path)
+    clip.audio.write_audiofile(temp_audio_path, logger=None)
+    temp_file = True
+    audio_path = temp_audio_path
+
+def play_audio():
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_path)
+    pygame.mixer.music.play()
+
+# pylint: disable=no-member
+cap = cv2.VideoCapture(video_path)
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+frame_duration = 1 / fps
+start_time = time.time()
+
+audio_thread = threading.Thread(target=play_audio)
+#audio_thread.daemon = True
+audio_thread.start()
+
 boxs = []
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
 
-    key = cv2.waitKey(1)
-    if key == 32:  # По нажатию 's' — распознать текст
-        result = ocr.ocr(frame, cls=cls)
-        lines = []
-        if not result[0]:
-            print("")
-        else:
-            new_boxs = []
-            for line in result[0]:
-                text = line[1][0]
-                lines.append((line[0][0][1], text))  # Y-координата + текст
+def recognize_text(frame):
+    global boxs
+    # Преобразуем кадр в черно-белое изображение для лучшего контраста
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    # Применение бинарного порога для улучшения контраста
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    result = ocr.ocr(thresh, cls=cls)
+    lines = []
+    if not result[0]:
+        print("❌ Не удалось распознать текст!")
+    else:
+        new_boxs = []
+        for line in result[0]:
+            text = line[1][0]
+            lines.append((line[0][0][1], text))  # Y-координата + текст
 
-                new_boxs.append(line[0])
-        # Преобразуем их в целые числа
-                
-                
-                # Рисуем рамку вокруг текста
-                
-            boxs = new_boxs
-            # Сортировка по Y (по вертикали)
-            lines.sort(key=lambda x: x[0])
+            new_boxs.append(line[0])
+            
+        boxs = new_boxs
+        # Сортировка по Y (по вертикали)
+        lines.sort(key=lambda x: x[0])
 
-            # Объединение в строку
-            full_text = ' '.join([text for _, text in lines])
-            print(full_text)
+        # Объединение в строку
+        full_text = ' '.join([text for _, text in lines])
+        print(full_text)
+
+
+with open("result.txt", "r+", encoding="utf-8") as f:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
         
-    if key == 13:
-        boxs = []
-    for box in boxs:
-        points = [(int(point[0]), int(point[1])) for point in box]
-        cv2.polylines(frame, [np.array(points)], isClosed=True, color=(0, 255, 0), thickness=2)
+        frame_num = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        time_sec = frame_num / fps
 
-    cv2.imshow("Video", frame)
-    if key == ord('q'):
-        break
+        # pylint: disable=no-member
+        key = cv2.waitKey(1)
+        if key == 32:
+            recognize_text(frame=frame)
+            
+        if key == 13:
+            boxs = []
+        for box in boxs:
+            points = [(int(point[0]), int(point[1])) for point in box]
+            cv2.polylines(frame, [np.array(points)], isClosed=True, color=(0, 255, 0), thickness=2)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow("Video", frame)
+        if key == ord('q'):
+            break
+        
+        elapsed = time.time() - start_time
+        expected = frame_num * frame_duration
+        delay = expected - elapsed
+        if delay > 0:
+            time.sleep(delay)
+
+    cap.release()
+    cv2.destroyAllWindows()
+    pygame.mixer.music.stop()
+
+    pygame.mixer.music.unload()
+        
+    if temp_file:
+        os.remove(audio_path)
